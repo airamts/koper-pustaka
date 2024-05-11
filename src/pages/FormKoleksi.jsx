f839f66a62f5d4a730eb2d7d1279bb61667af07f
mhdauldzikri <117631588+mhdauldzikri@users.noreply.github.com>
2024-05-11 20:46:04 +0700

    Add files via upload
    Menambahkan Page FormKoleksi, Data diri, dan Identitas

---
+++ src/pages/FormKoleksi.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FormKoleksi from "../components/formKoleksi-page/FormKoleksi";
+
+const FormKoleksiPage = () => {
+    return (
+        <div className="container-fluid">
+            <FormKoleksi />
+        </div>
+    )
+}
+
+export default FormKoleksiPage;
---
+++ src/pages/Identitas.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Identitas from "../components/Identitas-page/IdentitasApss";
+
+const IdentitasPage = () => {
+    return (
+        <div className="container-fluid">
+            <Identitas />
+        </div>
+    )
+}
+
+export default IdentitasPage;
