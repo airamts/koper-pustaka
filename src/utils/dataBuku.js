309eac8dcf217b492077e5288369e0d4ab008497
IRZAN-PC\hp <irzanraka@gmail.com>
2024-06-04 20:25:21 +0700

    tambah data buku

--- src/utils/dataBuku.js
+++ src/utils/dataBuku.js
@@ -96,7 +96,7 @@ const getBookData = () => [
     durationInMonths: 1,
     available: true,
     category: "Romance",
-    peminjam: 10,
+    peminjam: 2,
     antrian: 1,
     description:
       "Buku yang menurut saya sangat asik untuk dibaca, untuk kalian yang ingin membaca komik yang seru, saya rekomendasikan untuk baca ini. Saya berharap buku ini akan terus dilanjutkan. Biar makin banyak bibit-bibit orang hebat di negeri kita!",
@@ -117,54 +117,54 @@ const getBookData = () => [
         comment:
           "Pengalaman membaca yang sangat berkesan. Buku ini dalam kondisi sangat baik saat saya mengembalikannya.",
       },
-      {
-        id: 3,
-        avatar: "/images/default-user.png",
-        reviewer: "Citra Wibowo",
-        numberLoan: 3,
-        comment:
-          "Buku yang penuh dengan nilai-nilai kehidupan. Sangat direkomendasikan. Kondisinya sangat baik.",
-      },
-      {
-        id: 4,
-        avatar: "/images/default-user.png",
-        reviewer: "Dodi Pratama",
-        numberLoan: 4,
-        comment:
-          "Membaca buku ini memberikan wawasan baru. Sangat berharga. Buku dalam kondisi baik.",
-      },
-      {
-        id: 5,
-        avatar: "/images/default-user.png",
-        reviewer: "Eka Rahman",
-        numberLoan: 5,
-        comment:
-          "Sangat menginspirasi. Cerita yang mendalam dan penuh makna. Kondisi buku sangat baik.",
-      },
-      {
-        id: 6,
-        avatar: "/images/default-user.png",
-        reviewer: "Fina Rahayu",
-        numberLoan: 6,
-        comment:
-          "Buku ini memberikan banyak pelajaran hidup. Sangat bermanfaat. Kondisinya sangat baik.",
-      },
-      {
-        id: 7,
-        avatar: "/images/default-user.png",
-        reviewer: "Gilang Purnama",
-        numberLoan: 7,
-        comment:
-          "Cerita yang sangat menarik dan inspiratif. Buku dalam kondisi sangat baik saat saya mengembalikannya.",
-      },
-      {
-        id: 8,
-        avatar: "/images/default-user.png",
-        reviewer: "Hani Nuraini",
-        numberLoan: 8,
-        comment:
-          "Buku ini sangat memotivasi dan inspiratif. Saya sangat menikmatinya. Kondisi buku sangat baik.",
-      },
+      // {
+      //   id: 3,
+      //   avatar: "/images/default-user.png",
+      //   reviewer: "Citra Wibowo",
+      //   numberLoan: 3,
+      //   comment:
+      //     "Buku yang penuh dengan nilai-nilai kehidupan. Sangat direkomendasikan. Kondisinya sangat baik.",
+      // },
+      // {
+      //   id: 4,
+      //   avatar: "/images/default-user.png",
+      //   reviewer: "Dodi Pratama",
+      //   numberLoan: 4,
+      //   comment:
+      //     "Membaca buku ini memberikan wawasan baru. Sangat berharga. Buku dalam kondisi baik.",
+      // },
+      // {
+      //   id: 5,
+      //   avatar: "/images/default-user.png",
+      //   reviewer: "Eka Rahman",
+      //   numberLoan: 5,
+      //   comment:
+      //     "Sangat menginspirasi. Cerita yang mendalam dan penuh makna. Kondisi buku sangat baik.",
+      // },
+      // {
+      //   id: 6,
+      //   avatar: "/images/default-user.png",
+      //   reviewer: "Fina Rahayu",
+      //   numberLoan: 6,
+      //   comment:
+      //     "Buku ini memberikan banyak pelajaran hidup. Sangat bermanfaat. Kondisinya sangat baik.",
+      // },
+      // {
+      //   id: 7,
+      //   avatar: "/images/default-user.png",
+      //   reviewer: "Gilang Purnama",
+      //   numberLoan: 7,
+      //   comment:
+      //     "Cerita yang sangat menarik dan inspiratif. Buku dalam kondisi sangat baik saat saya mengembalikannya.",
+      // },
+      // {
+      //   id: 8,
+      //   avatar: "/images/default-user.png",
+      //   reviewer: "Hani Nuraini",
+      //   numberLoan: 8,
+      //   comment:
+      //     "Buku ini sangat memotivasi dan inspiratif. Saya sangat menikmatinya. Kondisi buku sangat baik.",
+      // },
     ],
   },
   {
@@ -264,7 +264,7 @@ const getBookData = () => [
     durationInMonths: 3,
     available: true,
     category: "Romance",
-    peminjam: 10,
+    peminjam: 4,
     antrian: 2,
     description:
       "Buku yang menurut saya sangat asik untuk dibaca, untuk kalian yang ingin membaca komik yang seru, saya rekomendasikan untuk baca ini. Saya berharap buku ini akan terus dilanjutkan. Biar makin banyak bibit-bibit orang hebat di negeri kita!",
@@ -301,40 +301,312 @@ const getBookData = () => [
         comment:
           "Pengalaman membaca yang sangat berkesan. Buku ini dalam kondisi sangat baik saat saya mengembalikannya.",
       },
-      {
-        id: 5,
-        avatar: "/images/default-user.png",
-        reviewer: "Umar Prasetyo",
-        numberLoan: 5,
-        comment:
-          "Buku ini penuh dengan cerita yang menginspirasi. Sangat direkomendasikan. Kondisinya sangat baik.",
-      },
-      {
-        id: 6,
-        avatar: "/images/default-user.png",
-        reviewer: "Vina Kurnia",
-        numberLoan: 6,
-        comment:
-          "Cerita yang sangat mendalam dan penuh makna. Kondisi buku sangat baik.",
-      },
-      {
-        id: 7,
-        avatar: "/images/default-user.png",
-        reviewer: "Winda Sari",
-        numberLoan: 7,
-        comment:
-          "Buku ini sangat menginspirasi dan emosional. Kondisinya sangat baik.",
-      },
-      {
-        id: 8,
-        avatar: "/images/default-user.png",
-        reviewer: "Xena Mahendra",
-        numberLoan: 8,
-        comment:
-          "Cerita yang sangat menyentuh dan inspiratif. Saya sangat menikmatinya. Kondisi buku sangat baik.",
-      },
+      // {
+      //   id: 5,
+      //   avatar: "/images/default-user.png",
+      //   reviewer: "Umar Prasetyo",
+      //   numberLoan: 5,
+      //   comment:
+      //     "Buku ini penuh dengan cerita yang menginspirasi. Sangat direkomendasikan. Kondisinya sangat baik.",
+      // },
+      // {
+      //   id: 6,
+      //   avatar: "/images/default-user.png",
+      //   reviewer: "Vina Kurnia",
+      //   numberLoan: 6,
+      //   comment:
+      //     "Cerita yang sangat mendalam dan penuh makna. Kondisi buku sangat baik.",
+      // },
+      // {
+      //   id: 7,
+      //   avatar: "/images/default-user.png",
+      //   reviewer: "Winda Sari",
+      //   numberLoan: 7,
+      //   comment:
+      //     "Buku ini sangat menginspirasi dan emosional. Kondisinya sangat baik.",
+      // },
+      // {
+      //   id: 8,
+      //   avatar: "/images/default-user.png",
+      //   reviewer: "Xena Mahendra",
+      //   numberLoan: 8,
+      //   comment:
+      //     "Cerita yang sangat menyentuh dan inspiratif. Saya sangat menikmatinya. Kondisi buku sangat baik.",
+      // },
     ],
   },
+    {
+      "id": 567,
+      "image": "/images/mermaid-1.png",
+      "image2": "/images/mermaid-2.png",
+      "image3": "/images/mermaid-3.png",
+      "title": "The Little Mermaid",
+      "location": "Cikarang",
+      "author": "H.C Andersen",
+      "owner": "@syifahui",
+      "avatar": "/images/default-user.png",
+      "durationInMonths": 2,
+      "available": true,
+      "category": "Adventure",
+      "peminjam": 8,
+      "antrian": 1,
+      "description": "Buku yang menurut saya sangat asik untuk dibaca, untuk kalian yang ingin membaca komik yang seru, saya rekomendasikan untuk baca ini. Saya berharap buku ini akan terus dilanjutkan. Biar makin banyak bibit-bibit orang hebat di negeri kita!",
+      "reviews": [
+        {
+          "id": 1,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Qori Hidayat",
+          "numberLoan": 1,
+          "comment": "Cerita yang sangat emosional dan menyentuh. Buku ini dalam kondisi sangat baik."
+        },
+        {
+          "id": 2,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Rina Sari",
+          "numberLoan": 2,
+          "comment": "Buku ini memberikan banyak pelajaran tentang kehidupan. Sangat direkomendasikan. Kondisi buku sangat baik."
+        },
+        {
+          "id": 3,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Sari Putri",
+          "numberLoan": 3,
+          "comment": "Cerita yang sangat inspiratif dan mendalam. Buku dalam kondisi sangat baik."
+        },
+        {
+          "id": 4,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Tomi Saputra",
+          "numberLoan": 4,
+          "comment": "Pengalaman membaca yang sangat berkesan. Buku ini dalam kondisi sangat baik saat saya mengembalikannya."
+        },
+        {
+          "id": 5,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Umar Prasetyo",
+          "numberLoan": 5,
+          "comment": "Buku ini penuh dengan cerita yang menginspirasi. Sangat direkomendasikan. Kondisinya sangat baik."
+        },
+        {
+          "id": 6,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Vina Kurnia",
+          "numberLoan": 6,
+          "comment": "Cerita yang sangat mendalam dan penuh makna. Kondisi buku sangat baik."
+        },
+        {
+          "id": 7,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Winda Sari",
+          "numberLoan": 7,
+          "comment": "Buku ini sangat menginspirasi dan emosional. Kondisinya sangat baik."
+        },
+        {
+          "id": 8,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Xena Mahendra",
+          "numberLoan": 8,
+          "comment": "Cerita yang sangat menyentuh dan inspiratif. Saya sangat menikmatinya. Kondisi buku sangat baik."
+        }
+      ]
+    },
+    {
+      "id": 678,
+      "image": "/images/book-asmuch.png",
+      "image2": "/images/book-asmuch.png",
+      "image3": "/images/book-asmuch.png",
+      "title": "As Much As I Love You",
+      "location": "Pasar Minggu",
+      "author": "Wulan K",
+      "owner": "@marwah",
+      "avatar": "/images/default-user.png",
+      "durationInMonths": 1,
+      "available": true,
+      "category": "Romance",
+      "peminjam": 4,
+      "antrian": 2,
+      "description": "Buku yang menurut saya sangat asik untuk dibaca, untuk kalian yang ingin membaca komik yang seru, saya rekomendasikan untuk baca ini. Saya berharap buku ini akan terus dilanjutkan. Biar makin banyak bibit-bibit orang hebat di negeri kita!",
+      "reviews": [
+        {
+          "id": 1,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Qori Hidayat",
+          "numberLoan": 1,
+          "comment": "Cerita yang sangat emosional dan menyentuh. Buku ini dalam kondisi sangat baik."
+        },
+        {
+          "id": 2,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Rina Sari",
+          "numberLoan": 2,
+          "comment": "Buku ini memberikan banyak pelajaran tentang kehidupan. Sangat direkomendasikan. Kondisi buku sangat baik."
+        },
+        {
+          "id": 3,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Sari Putri",
+          "numberLoan": 3,
+          "comment": "Cerita yang sangat inspiratif dan mendalam. Buku dalam kondisi sangat baik."
+        },
+        {
+          "id": 4,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Tomi Saputra",
+          "numberLoan": 4,
+          "comment": "Pengalaman membaca yang sangat berkesan. Buku ini dalam kondisi sangat baik saat saya mengembalikannya."
+        },
+      ]
+    },
+    {
+      "id": 789,
+      "image": "/images/book-spring.png",
+      "image2": "/images/book-spring.png",
+      "image3": "/images/book-spring.png",
+      "title": "Spring In London",
+      "location": "Batu Ceper",
+      "author": "Ilana Tan",
+      "owner": "@megawai",
+      "avatar": "/images/default-user.png",
+      "durationInMonths": 2,
+      "available": true,
+      "category": "Romance",
+      "peminjam": 6,
+      "antrian": 1,
+      "description": "Buku yang menurut saya sangat asik untuk dibaca, untuk kalian yang ingin membaca komik yang seru, saya rekomendasikan untuk baca ini. Saya berharap buku ini akan terus dilanjutkan. Biar makin banyak bibit-bibit orang hebat di negeri kita!",
+      "reviews": [
+        {
+          "id": 1,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Qori Hidayat",
+          "numberLoan": 1,
+          "comment": "Cerita yang sangat emosional dan menyentuh. Buku ini dalam kondisi sangat baik."
+        },
+        {
+          "id": 2,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Rina Sari",
+          "numberLoan": 2,
+          "comment": "Buku ini memberikan banyak pelajaran tentang kehidupan. Sangat direkomendasikan. Kondisi buku sangat baik."
+        },
+        {
+          "id": 3,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Sari Putri",
+          "numberLoan": 3,
+          "comment": "Cerita yang sangat inspiratif dan mendalam. Buku dalam kondisi sangat baik."
+        },
+        {
+          "id": 4,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Tomi Saputra",
+          "numberLoan": 4,
+          "comment": "Pengalaman membaca yang sangat berkesan. Buku ini dalam kondisi sangat baik saat saya mengembalikannya."
+        },
+        {
+          "id": 5,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Umar Prasetyo",
+          "numberLoan": 5,
+          "comment": "Buku ini penuh dengan cerita yang menginspirasi. Sangat direkomendasikan. Kondisinya sangat baik."
+        },
+        {
+          "id": 6,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Vina Kurnia",
+          "numberLoan": 6,
+          "comment": "Cerita yang sangat mendalam dan penuh makna. Kondisi buku sangat baik."
+        },
+        // {
+        //   "id": 7,
+        //   "avatar": "/images/default-user.png",
+        //   "reviewer": "Winda Sari",
+        //   "numberLoan": 7,
+        //   "comment": "Buku ini sangat menginspirasi dan emosional. Kondisinya sangat baik."
+        // },
+        // {
+        //   "id": 8,
+        //   "avatar": "/images/default-user.png",
+        //   "reviewer": "Xena Mahendra",
+        //   "numberLoan": 8,
+        //   "comment": "Cerita yang sangat menyentuh dan inspiratif. Saya sangat menikmatinya. Kondisi buku sangat baik."
+        // }
+      ]
+    },
+    {
+      "id": 891,
+      "image": "/images/book-mermaid.png",
+      "image2": "/images/book-mermaid.png",
+      "image3": "/images/book-mermaid.png",
+      "title": "Plain Jane and the Mermaid",
+      "location": "Bekasi",
+      "author": "Maia",
+      "owner": "@stayfetters",
+      "avatar": "/images/default-user.png",
+      "durationInMonths": 3,
+      "available": true,
+      "category": "Animasi",
+      "peminjam": 8,
+      "antrian": 3,
+      "description": "Buku yang menurut saya sangat asik untuk dibaca, untuk kalian yang ingin membaca komik yang seru, saya rekomendasikan untuk baca ini. Saya berharap buku ini akan terus dilanjutkan. Biar makin banyak bibit-bibit orang hebat di negeri kita!",
+      "reviews": [
+        {
+          "id": 1,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Qori Hidayat",
+          "numberLoan": 1,
+          "comment": "Cerita yang sangat emosional dan menyentuh. Buku ini dalam kondisi sangat baik."
+        },
+        {
+          "id": 2,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Rina Sari",
+          "numberLoan": 2,
+          "comment": "Buku ini memberikan banyak pelajaran tentang kehidupan. Sangat direkomendasikan. Kondisi buku sangat baik."
+        },
+        {
+          "id": 3,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Sari Putri",
+          "numberLoan": 3,
+          "comment": "Cerita yang sangat inspiratif dan mendalam. Buku dalam kondisi sangat baik."
+        },
+        {
+          "id": 4,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Tomi Saputra",
+          "numberLoan": 4,
+          "comment": "Pengalaman membaca yang sangat berkesan. Buku ini dalam kondisi sangat baik saat saya mengembalikannya."
+        },
+        {
+          "id": 5,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Umar Prasetyo",
+          "numberLoan": 5,
+          "comment": "Buku ini penuh dengan cerita yang menginspirasi. Sangat direkomendasikan. Kondisinya sangat baik."
+        },
+        {
+          "id": 6,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Vina Kurnia",
+          "numberLoan": 6,
+          "comment": "Cerita yang sangat mendalam dan penuh makna. Kondisi buku sangat baik."
+        },
+        {
+          "id": 7,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Winda Sari",
+          "numberLoan": 7,
+          "comment": "Buku ini sangat menginspirasi dan emosional. Kondisinya sangat baik."
+        },
+        {
+          "id": 8,
+          "avatar": "/images/default-user.png",
+          "reviewer": "Xena Mahendra",
+          "numberLoan": 8,
+          "comment": "Cerita yang sangat menyentuh dan inspiratif. Saya sangat menikmatinya. Kondisi buku sangat baik."
+        }
+      ]
+    }
 ];
 
 export { getBookData };
